8b6c0058f80ba24e4d2520dbc2e1e8825ad939df
RN-studio <life7die4@gmail.com>
2024-01-06 21:27:20 +0900

    "app.py 버그 수정"

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ df = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
 
 conn.close()
 
+
 @app.route('/sitemap.xml')
 @app.route('/robots.txt')
 @app.route('/ads.txt')
 def robot_to_root():
     return send_from_directory(app.static_folder, request.path[1:])
+
 
 @app.route('/')
 def index():
@@ -30,7 +32,6 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-
     del_number = []
     for i in range(1, 7):
         try:
@@ -43,16 +44,13 @@ def result():
             pass
 
     num_of_games = int(request.form['game_count']) * 5
-    weight_option =int(request.form['weight_option'])
-
+    weight_option = int(request.form['weight_option'])
 
     # 번호 삭제
     if len(del_number) >= 1:
         df_temp = df.drop(index=del_number)
     else:
         df_temp = df
-
-
 
     # 가중치 계산
     if weight_option == 1:
@@ -67,12 +65,7 @@ def result():
 
     # 로또 번호 추출 및 정렬
     results = []
-    if weight_option == 1:
-        for game in range(1, num_of_games + 1):
-            selected_numbers = random.sample(sorted(df_temp['Value'].tolist(), key=lambda x: random.choice(weights)),
-                                             k=6)
-            results.append({"game": game, "numbers": sorted(selected_numbers)})
-    elif weight_option == 2:
+    if (weight_option == 1) or (weight_option == 2):
         for game in range(1, num_of_games + 1):
             selected_numbers = random.sample(sorted(df_temp['Value'].tolist(), key=lambda x: random.choice(weights)),
                                              k=6)
